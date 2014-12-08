f2a469f60357d10a3aac04ed26c173ba0502aa46
Dana Mulder <danarmulder@gmail.com>
2014-12-08 10:06:37 -0800

    add create button to the index page for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,20 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Products.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 end
